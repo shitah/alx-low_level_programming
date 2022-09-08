62974c0573e9261bd3a0421705869ebc9b134a49
shitah <shitahun2012@gmail.com>
2022-09-08 13:24:39 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -first programming
+ *
+ * Return: Always 0 (Great)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
