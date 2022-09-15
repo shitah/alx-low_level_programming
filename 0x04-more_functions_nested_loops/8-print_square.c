304049f641731c470c1f32578f2b147d4a7c4ac1
shitah <shitahun2012@gmail.com>
2022-09-15 15:36:09 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,26 @@
 #include "main.h"
 
 /**
- *  * main - check the code for ALX School students.
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
+ * print_square - check for a digit
+ * @n: number of _ to be printed
+ * Return: void
+ */
+
+void print_square(int n)
 {
-	    print_square(2);
-	        print_square(10);
-		    print_square(0);
-		        return (0);
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
 }
