7df0da89a0cb7c703c5587e1dbe5d2972a624b2a
shitah <shitahun2012@gmail.com>
2022-09-20 14:19:51 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,43 @@
 #include "main.h"
 
+
+
 /**
- * puts2 - prints every other character of a
- * string, starting with the first character
  *
- * @str: the used string reference
- * Return: 0
- */
+ *  * puts2 - prints xters of a string
+ *
+ *   * @str: the used string reference
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
 
 void puts2(char *str)
-{
-	int i = 0;
 
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
-	}
-	_putchar('\n');
+{
+
+		int i = 0;
+
+
+
+			while (str[i] != '\0')
+
+					{
+
+								if (i % 2 == 0)
+
+											{
+
+															_putchar(str[i]);
+
+																	}
+
+										i++;
+
+											}
+
+				_putchar('\n');
+
 }
