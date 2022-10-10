c12f4a8624dfe8481f40bb6a8a9399e2c46071bd
shitah <shitahun2012@gmail.com>
2022-10-10 13:52:50 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file it was 
+ * compiled from followes by a new line
+ * Return: 0 if success
+ */
+
+int main (void)
+{
+	printf (%s\n,_FILE_);
+	return (0);	
+}
