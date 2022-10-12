996f4e702d2564f25b88f932315beeb5fc9485c8
shitah <shitahun2012@gmail.com>
2022-10-12 15:12:06 -0700

    mandatery tasks for 0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,18 @@
-#include <stdlib>
 #include "function_pointers.h"
-
 /**
- * array_interator - executes a function given as a parameter
- * on each element of an array
- * @array: array to interat over
- * @size: size of the array
- * @action: pointer to function used
+ * array_iterator - execute a function on array's element
+ *  @array: array
+ *  @size: size of the array
+ *  @action: function to execute
+ *  Return: nothing
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i = 0;
 
-	if (!array || !action)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
 }
